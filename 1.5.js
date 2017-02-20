b257d794c63ac8a08ef1f52b383593ac7e8a3436
Justyna Foster <justynawfoster@gmail.com>
2017-02-19 18:42:43 -0700

    initial commit

--- 1.5.js
+++ 1.5.js
@@ -3,18 +3,22 @@
 let numbers =[1, 2, 3, 4, 5, 6, 7];
 let myNumber = 13;
 
-function targetValue(arr, int) {
-    let result =[];
-    for (var i = 0; i < arr.length; i++) {
-      for (let j = arr.length; j > 0; j--) {
-        if (arr[i] + arr[j] === int) {
-          result.push(arr[i]);
-        }
-      }
-    }
-    return result;
-  }
 
-  console.log(targetValue(numbers, myNumber));
+function twoSums(arr, int){
+  //find two different items in array
+  //when added gives 'int'
+  //return arr of these numbers
+  //input: [1, 2, 3, 4, 5, 6, 7], 13
+  //output: [5,6]
 
-module.exports = targetValue;
+
+}
+
+
+
+
+
+
+
+
+
